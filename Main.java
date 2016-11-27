803dd54a0de7ac463a08c91b088ccae415d8404b
rostik26 <rostuslavsuhov@gmail.com>
2016-11-27 22:17:51 +0200

    homework

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+        for (int g = 10; g >= -10; g--) {
+          System.out.println(g);
+        }
+        for (int r = 1; r < 33; r= r+2) {
+            System.out.println(r);
+
+        }
+    }
+}
